9a6837d326bbe733c29bd906d3450d354dff9628
Yakow <weber@neolant.ru>
2019-09-19 11:13:22 +0200

    add layout and base class

--- app/src/main/java/com/weber/testc/MainActivity.kt
+++ app/src/main/java/com/weber/testc/MainActivity.kt
@@ -5,14 +5,37 @@ import android.widget.ArrayAdapter
 import androidx.appcompat.app.AppCompatActivity
 import com.weber.testc.model.StoreType
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var keyPattern: Pattern
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        keyPattern = Pattern.compile("\\p{Alnum}+")
         typeSpinner.adapter =
             ArrayAdapter<StoreType>(this, android.R.layout.simple_spinner_item, StoreType.values())
+
+        getValueButton.setOnClickListener {
+            getValueStore()
+        }
+
+        setValueButton.setOnClickListener {
+            setValueStore()
+        }
+
+
+
+    }
+
+    private fun setValueStore() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun getValueStore() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
 }
--- app/src/main/java/com/weber/testc/model/StoreType.kt
+++ app/src/main/java/com/weber/testc/model/StoreType.kt
@@ -1,10 +1,13 @@
 package com.weber.testc.model
 
+import java.util.*
+
 /**
  * Created on 9/19/2019
  * @author YWeber
  */
 
 enum class StoreType {
-    TEXT
+    TEXT, INT, DATE;
+
 }
